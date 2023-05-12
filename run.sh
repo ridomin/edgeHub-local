b74938352201713c9d594eb28bc19e0da6be72f3
Rido <ridomin@live.com>
2023-05-12 09:13:39 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 hub=rido
 edgeId=tr-gateway
 connStr=$(az iot hub module-identity connection-string show -n $hub -d $edgeId -m '$edgeHub' --query connectionString | tr -d '"')
-docker run -it --rm -e IotHubConnectionString="$connStr" -p 8883:8883 ghcr.io/ridomin/edgehub:local
+docker run -it --rm -e IotHubConnectionString="$connStr" -e RuntimeLogLevel=debug -p 8883:8883 ghcr.io/ridomin/edgehub:local
