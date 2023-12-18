6a73607932b7008af9e64538cb36d0c6b9c5219b
ridomin <ridomin@live.com>
2023-12-18 00:20:23 -0800

    add certs

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
-hub=rido
-edgeId=tr-gateway
-connStr=$(az iot hub module-identity connection-string show -n $hub -d $edgeId -m '$edgeHub' --query connectionString | tr -d '"')
-docker run -it --rm -e IotHubConnectionString="$connStr" -p 8883:8883 ghcr.io/ridomin/edgehub:local
+hub=tests
+edgeId=e1
+#connStr=$(az iot hub module-identity connection-string show -n $hub -d $edgeId -m '$edgeHub' --query connectionString | tr -d '"')
+connStr='HostName=tests.azure-devices.net;DeviceId=e1;ModuleId=$edgeHub;SharedAccessKey=L3kmGFECSSSzJBsBLFwAFXjEjHK+ZqnP+iQy7rXxkl8='
+
+ #dotnet dev-certs https -ep _certs/localhost.pem --format PEM --no-password
+ #cp _certs/localhost.pem _certs/ca.pem
+
+docker run -it --rm \
+    -e IotHubConnectionString="$connStr" \
+    -e EdgeModuleHubServerCertificateFile=/certs/localhost.pem \
+    -e EdgeModuleHubServerCAChainCertificateFile=/certs/ca.pem \
+    -e EdgeHubDevServerCertificateFile=/certs/localhost.pem \
+    -e EdgeHubDevTrustBundleFile=/certs/ca.pem \
+    -e EdgeHubDevServerPrivateKeyFile=/certs/localhost.key \
+    -v $(pwd)/_certs:/certs \
+    -p 8883:8883 \
+    mcr.microsoft.com/azureiotedge-hub:1.4
